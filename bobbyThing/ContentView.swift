d407b7e102bfda99d9be35148c78db7d7ef6f17f
Sharlene Tan Qin Ying <sharlene_tan_qin_ying@s2024.ssts.edu.sg>
2025-08-02 09:51:25 +0800

    Initial Commit

---
+++ bobbyThing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bobbyThing
+//
+//  Created by Sharlene Tan Qin Ying on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bobbyThing/bobbyThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  bobbyThingApp.swift
+//  bobbyThing
+//
+//  Created by Sharlene Tan Qin Ying on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bobbyThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
